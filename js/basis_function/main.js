f3bfe50137bec30d4815a1296270d23cc6450e63
Bertrand Thierry <bthierry@math.cnrs.fr>
2019-08-12 15:07:10 +0200

    js basis fun: triangle obtaind by transform + grad color

--- js/basis_function/main.js
+++ js/basis_function/main.js
@@ -1,24 +1,38 @@
 var data_pts = [
-    {'x':0,   'y': 0,  'tri':[]},
-    {'x':140, 'y': 10, 'tri':[]},
-    {'x':100, 'y': 80, 'tri':[]},
-    {'x':-10, 'y': 120,'tri':[]},
-    {'x':45,  'y': 50, 'tri':[]},
-    {'x':150, 'y': 130,'tri':[]} 
+    {'x':0,   'y': 0,  'tri':[], 'local':[]},
+    {'x':140, 'y': 10, 'tri':[], 'local':[]},
+    {'x':100, 'y': 80, 'tri':[], 'local':[]},
+    {'x':-10, 'y': 120,'tri':[], 'local':[]},
+    {'x':45,  'y': 50, 'tri':[], 'local':[]},
+    {'x':150, 'y': 130,'tri':[], 'local':[]} 
 ];
 var mesh_triangles= [[0,1,4], [4,1,2], [3, 4, 2], [3,0,4], [2,3,5], [1,2,5]];
-
 // for each point, find the triangles it belongs to
 mesh_triangles.forEach(function(t, i) {
-    t.forEach(function(pt){
+    t.forEach(function(pt, loc){
         data_pts[pt].tri.push(i);
+        data_pts[pt].local.push(loc);
     })
 });
 
+// Reference triangle
+var ref_pts = [
+    {'x':0, 'y': 0},
+    {'x':1, 'y': 0},
+    {'x':0, 'y': 1}
+];
+var middle_pts = [
+    {'x':0.5, 'y': 0.5},
+    {'x':0, 'y': 0},
+    {'x':0, 'y': 0}
+]
+var ref_triangle = [0,1,2];
 
 var triangle_color = {
     "inactive": "none", 
-    "active": "GhostWhite"
+    "active": "GhostWhite",
+    "zero": "green",
+    "one": "gold"
 };
 var point_color = {
     "active": "darkblue",
@@ -56,8 +70,21 @@ var svg = div.insert('svg', ":first-child")
                 .attr('width', size_x)
                 .attr('height', size_y)
                 .attr('style', 'width:75%; height:auto;')
-                ;
-                
+;
+// Color Gradient
+var grad = svg.append('defs')
+            .selectAll('linearGradient')
+            .data(ref_triangle)
+            .enter()
+            .append('linearGradient')
+            .attr('id', d=> "gradtri-" + d)
+            .attr('x1', d=>middle_pts[d].x)
+            .attr('x2', d=>ref_pts[d].x)
+            .attr('y1', d=>middle_pts[d].y)
+            .attr('y2', d=>ref_pts[d].y)
+;
+svg.selectAll('linearGradient').append('stop').attr('offset', "0%").attr('stop-color', triangle_color.zero);
+svg.selectAll('linearGradient').append('stop').attr('offset', "95%").attr('stop-color', triangle_color.one);
 
 var all_triangles = svg.append('g')
     .attr('class','all_triangles')
@@ -65,12 +92,22 @@ var all_triangles = svg.append('g')
     .data(mesh_triangles)
     .enter()
     .append('polygon')
-    .attr('points', function(d){return data_pts[d[0]].x + " " + data_pts[d[0]].y + ", " + data_pts[d[1]].x + " " + data_pts[d[1]].y + ", " + data_pts[d[2]].x + " " + data_pts[d[2]].y;})
     .attr('class', function(d,i){ return 'd3-triangle d3-triangle-' + i;})
     .attr('fill', 'none')
     .attr('pointer-events', 'fill')
     .attr('stroke', 'black')
+    .attr('stroke-width', '2px')
     .attr('active', '0')
+    .attr('points', '0 0, 1 0, 0 1')
+    .attr('transform', function(d){
+        let aa = parseFloat(data_pts[d[1]].x - data_pts[d[0]].x);
+        let bb = parseFloat(data_pts[d[1]].y - data_pts[d[0]].y);
+        let cc = parseFloat(data_pts[d[2]].x - data_pts[d[0]].x);
+        let dd = parseFloat(data_pts[d[2]].y - data_pts[d[0]].y);
+        let ee = data_pts[d[0]].x;
+        let ff = data_pts[d[0]].y;
+        return "matrix(" + aa + " " + bb + " " + cc + " " + dd + " " + ee + " " + ff + ")";})
+    .attr('vector-effect', 'non-scaling-stroke')
     ;
 
 // Build all vertices
@@ -136,11 +173,11 @@ function activate_vertex(t){
 
 function activate_triangles(t, d){
     svg.selectAll('.d3-triangle')
-        .attr('fill', triangle_color.inactive)
+        .attr('fill', triangle_color.zero)
     ;
-    d.tri.forEach(function(i) {
+    d.tri.forEach(function(i, index) {
         svg.select('.d3-triangle-'+i)
-        .attr('fill', triangle_color.active)
+        .attr('fill', 'url(#gradtri-'+ d.local[index]+')')
         .attr('stroke-opacity', '1')
         .attr('active', "1")
     });
@@ -163,9 +200,14 @@ function activate(t, d, i){
     {
         activate_vertex(t, d);
         activate_triangles(t,d);
-        title.text('Support de la fonction de forme du sommet ' + i);
+        title.text('Fonction de forme du sommet ' + i);
     }
     else{
         title.text('Maillage');
     }
+}
+
+function p1(i, x, y){
+
+
 }
